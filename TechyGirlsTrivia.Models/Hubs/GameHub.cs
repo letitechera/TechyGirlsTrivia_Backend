ea84dfc4bd266ae2195fdb0b878b026bb03251c8
Leticia <leticiat@onetree.com>
2019-04-10 14:20:36 -0300

    Changes

--- TechyGirlsTrivia.Models/Hubs/GameHub.cs
+++ TechyGirlsTrivia.Models/Hubs/GameHub.cs
@@ -2,10 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TechyGirlsTrivia.Models.Models;
 
 namespace TechyGirlsTrivia.Models.Hubs
 {
     public class GameHub: Hub
     {
+        public async Task BroadcastChartData(List<Question> data) => await Clients.All.SendAsync("broadcastchartdata", data);
     }
 }
