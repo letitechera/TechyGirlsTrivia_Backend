aa91d19ae40169bed52441c4dcb2fccaf7ab7327
Leticia <leticiat@onetree.com>
2019-04-10 12:39:59 -0300

    Init Solution

---
+++ TechyGirlsTrivia.Models/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TechyGirlsTrivia.Models.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+    }
+}
---
+++ TechyGirlsTrivia.WebAPI/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using TechyGirlsTrivia.Models.Helpers;
+using TechyGirlsTrivia.Models.Hubs;
+using TechyGirlsTrivia.Models.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TechyGirls.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameController : Controller
+    {
+        private IHubContext<GameHub> _hub;
+
+        public GameController(IHubContext<GameHub> hub)
+        {
+            _hub = hub;
+        }
+
+        // GET: api/<controller>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var timerManager = new TimerManager(() => _hub.Clients.All.SendAsync("transferdata", new Question { QuestionId = 0, QuestionText = "What's your Name?" }));
+            return Ok(new { Message = "Request Completed" });
+        }
+    }
+}
