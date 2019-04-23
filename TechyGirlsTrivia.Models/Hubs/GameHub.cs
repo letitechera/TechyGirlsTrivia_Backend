a38654303d446318988ebfd4a2bba478544c08c5
Leticia Techera <leticiat@onetree.com>
2019-04-22 22:54:14 -0300

    FIx

--- TechyGirlsTrivia.Models/Hubs/GameHub.cs
+++ TechyGirlsTrivia.Models/Hubs/GameHub.cs
@@ -9,8 +9,6 @@ namespace TechyGirlsTrivia.Models.Hubs
 {
     public class GameHub: Hub
     {
-        public async Task BroadcastQuestion(Question data) => await Clients.All.SendAsync("showquestion", data);
-        public async Task BroadcastNewParticipant(bool data) => await Clients.All.SendAsync("newparticipant", data);
         public async Task BroadcastStart(bool data) => await Clients.All.SendAsync("broadcastStart", data);
 
         public string GetConnectionId()
